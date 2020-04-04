ad09c4841c07cb47b05ddb110387296ea95dd999
noah <gaoyong@xatu.edu.cn>
2020-04-04 22:51:20 +0800

    2020-0404-构建课程模型-v1

--- educa/settings.py
+++ educa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_56m=*g32yj2cv65*!+g8nnxtj+ekcwq&)0+g&x=tn8i$p01w^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['222.25.2.37']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
